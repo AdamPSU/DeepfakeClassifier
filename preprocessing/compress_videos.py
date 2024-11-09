4d6c8081dcc4fc7b565d20ccae85d0c8ea022c8e
Adam Torres Encarnacion <admr0805@gmail.com>
2024-11-09 03:55:11 -0500

    Generalize to all training videos

--- preprocessing/compress_videos.py
+++ preprocessing/compress_videos.py
@@ -1,13 +1,16 @@
 import random
-
+import logging
 from moviepy.editor import VideoFileClip
 from pathlib import Path
 
-# Feel free to modify these to suit your needs
-DEFAULT_INPUT_DIR = "../data/train_sample_videos/"
-DEFAULT_OUTPUT_DIR = "../data/compressed"
+# Set the logging level to ERROR to suppress MoviePy's INFO and DEBUG messages
+logging.getLogger("moviepy").setLevel(logging.CRITICAL)
 
-def compress_video(input_dir, output_dir, video):
+# Feel free to modify these to suit your needs
+DEFAULT_INPUT_DIR = Path("../data/train_sample_videos/")
+DEFAULT_OUTPUT_DIR = Path("../data/compressed")
+
+def compress_video(input_path, output_dir):
     """
     This function compresses .mp4's using the moviepy module.
 
@@ -19,19 +22,30 @@ def compress_video(input_dir, output_dir, video):
 
     # If the output folder doesn't exist, make it.
     # Note that the input folder MUST exist, as it contains the video.
-    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-    input_path = f"{input_dir}/{video}"
-    output_path = f"{output_dir}/{video}"
+    video_name = input_path.name
+    output_path = output_dir / video_name # Add video to the output folder
+
+    # Convert from Path objects to Python strings
+    input_as_string = input_path.as_posix()
+    output_as_string = output_path.as_posix()
 
     # Choose between fast and ultrafast presets. This yields lower quality, but faster videos.
     compression_level = random.choice(["fast", "ultrafast"])
 
-    video_clip = VideoFileClip(input_path)
+    video_clip = VideoFileClip(input_as_string)
     video_clip.write_videofile(
-        output_path,
-        codec="libx264",  # Use H.264 video codec
+        output_as_string,
+        codec="libx264",  # Use H.264 video codec for compression
         preset=compression_level,
         audio=False # I assume we won't be needing audio?
     )
 
+# TODO: this entire thing should be multi-processed
+if __name__ == '__main__':
+    # Get all videos in input directory by matching all .mp4 files with glob.
+    all_videos = DEFAULT_INPUT_DIR.glob("*.mp4")
+
+    for video_path in all_videos:
+        compress_video(video_path, DEFAULT_OUTPUT_DIR)
